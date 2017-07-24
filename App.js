ed20f5787e0b46e58138351d6f9e29d8516a493e
dung.ngo <dung.ngo@niteco.se>
2017-07-24 11:06:35 +0700

    render todo list

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import Todo from './src/js/todo';
 
 export default class App extends React.Component {
   componentWillMount() {
@@ -7,20 +8,7 @@ export default class App extends React.Component {
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+        <Todo />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/js/todo.js
+++ src/js/todo.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView } from 'react-native';
+import { 
+    StyleSheet, 
+    Text, 
+    ScrollView, 
+    View, 
+    TouchableHighlight } from 'react-native';
+import Data from '../data/data.json';
+import {
+  MKCheckbox,
+} from 'react-native-material-kit';
 
 export default class Todo extends React.Component {
-    constructor() {
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -11,24 +20,23 @@ export default class Todo extends React.Component {
         };
 
         this.getTasks = this.getTasks.bind(this);
+        this.create = this.create.bind(this);
+        this.changeStatus = this.changeStatus.bind(this);
     }
 
     componentWillMount() {
-        getTasks();
+        this.getTasks();
     }
 
     getTasks() {
-        fetch('../data/data.json')
-            .then((response) => response.json())
-            .then((responseJson) => {
-                this.setState({
-                    loading: false,
-                    tasks: responseJson.tasks
-                });
+        if(Data) {
+            this.setState({
+                loading: true,
+                tasks: Data.tasks
             })
-            .catch((error) => {
-                console.error(error);
-            });
+        } else {
+            getTasks();
+        }
     }
 
     create() {
@@ -43,22 +51,66 @@ export default class Todo extends React.Component {
         
     }
 
+    changeStatus() {
+        
+    }
+
     render() {
         const { loading, tasks } = this.state;
-
         return (
-            <View>
-                { tasks && tasks.map(task => {
-                        <View>
-                            <Text>task.content</Text>
-                        </View>
-                    })
-                }
+            <View style={{flex: 1}}>
+                <View style={styles.todoButtonWrapper}>
+                    <TouchableHighlight 
+                        onPress={ () => this.create }>
+                        <Text style={styles.button}>Add new</Text>
+                    </TouchableHighlight>
+                </View>
+
+                <ScrollView contentContainerStyle={styles.todoContainer}>
+                    { tasks && tasks.map(task =>
+                            <View key={task.id} style={styles.taskItem}>
+                                <Text style={[styles.contentStyle, styles.fontStyle]}>{task.id}. {task.content}</Text>
+                                
+                            </View>
+                        )
+                    }
+                </ScrollView>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-
+    todoContainer: {
+        backgroundColor: '#F5FCFF'
+    },
+    taskItem: {
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        flexDirection: 'row'
+    },
+    contentStyle: {
+        padding: 10,
+        margin: 0,
+        flex: 0.8
+    },
+    fontStyle: {
+        fontSize: 18,
+        color: '#222',
+    },
+    todoButtonWrapper: {
+        marginTop: 35,
+        padding: 5,
+        flexDirection: 'row'
+    },
+    button: {
+        padding: 20,
+        backgroundColor: '#222',
+        color: '#fff',
+        marginLeft: 5,
+        width: 100
+    },
+    checkBox: {
+        borderWidth: 0
+    }
 })
